da1f7dfb6bdad92d3c381f1ea3eb1b823586ba1a
abilgaiyan <abilgaiyan@gmail.com>
2018-10-22 16:22:56 +0530

    fixed the data export

--- client/src/components/roles/RoleList.js
+++ client/src/components/roles/RoleList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //import { List, Datagrid, TextField, BooleanField  } from 'react-admin';
-import { List, Edit, Create, Datagrid,BooleanField, BooleanInput , ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'react-admin';
+import { List, downloadCSV ,Edit, Create, Datagrid,BooleanField, BooleanInput , ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'react-admin';
+import { unparse as convertToCSV } from 'papaparse/papaparse.min';
 import {RoleNameField} from './RoleNameField'
 
 export const RoleList = (props) => (
-    <List  {...props}>
+    <List  {...props} exporter={exporter}>
         <Datagrid>
             <TextField source="rolename" label="RoleName" />
             <RoleNameField  label="UserName" />
@@ -59,3 +60,23 @@ export const RoleCreate = (props) => (
         </SimpleForm>
     </Create>
 );
+
+const exporter = roles => {
+    const rolesForExport = roles.map(role => {
+        // add a field from an embedded resource
+        role.permissions_dataupload = role.permissions.dataupload;
+        role.permissions_settings = role.permissions.settings;
+        role.permissions_newusers = role.permissions.newusers;
+        role.permissions_datadownload = role.permissions.datadownload;
+        role.permissions_changepermission = role.permissions.changepermission;
+        role.permissions_createstudy = role.permissions.createstudy;
+        role.permissions_createbattery = role.permissions.createbattery;
+        return role;
+    });
+    const csv = convertToCSV({
+        data: rolesForExport,
+        // select and order fields in the export
+        fields: ['id', 'rolename', 'username', 'permissions_dataupload', 'permissions_settings', 'permissions_newusers', 'permissions_datadownload', 'permissions_changepermission', 'permissions_createstudy', 'permissions_createbattery']
+    });
+    downloadCSV(csv, 'roles'); // download as 'roles.csv` file
+}
